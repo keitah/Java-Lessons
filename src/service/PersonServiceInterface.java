09d69b196e76a994a99817f0101279dcc7aa226f
Artem <keitaaa@bk.ru>
2023-04-15 19:11:03 +0300

    V.1.0

--- src/service/PersonServiceInterface.java
+++
@@ -1,8 +0,0 @@
-//v1
-package service;
-public interface PersonServiceInterface {
-    void addPerson(String name, int age);
-    void updatePerson(int id, String name, int age);
-    void deletePerson(int id);
-    void listPeople();
-}
